05c780407c938eb0b97681c93a79dae4a005d97b
Gowtham-015 <gowthamchowdarync@gmail.com>
2026-02-06 16:35:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const nav = document.getElementById("navLinks");
+  nav.style.display = nav.style.display === "flex" ? "none" : "flex";
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  alert("Thank you! Your message has been sent.");
+}
